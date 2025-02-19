2eca207ccc5150d72d930cb9028b0ec6affa3804
marilynbraojos <marilynbraojos@gmail.com>
2025-02-19 14:48:49 -0500

    :white_check_mark: implemented proportional controller and timeout

--- install/ironman_chase_object/lib/python3.10/site-packages/ironman_chase_object/velocity_controller.py
+++ install/ironman_chase_object/lib/python3.10/site-packages/ironman_chase_object/velocity_controller.py
@@ -15,7 +15,7 @@ import sys
 
 
 class VelocityController(Node):
-     def __init__(self):        
+    def __init__(self):        
         super().__init__('velocity_publisher')
 
         self._pixel_subscriber = self.create_subscription(
@@ -27,19 +27,40 @@ class VelocityController(Node):
         
         self._vel_publish = self.create_publisher(Twist, '/cmd_vel', 10)
 
-     def pixel_callback(self, msg: Point):
 
+        self.Kp = 0.005  # proportional gain 
+        self.max_angular_speed = 1.0 
+        self.dead_zone = 10  # Pixels within which we don't rotate
+
+
+        self.last_msg_time = self.get_clock().now()  # Track last received message
+        self.timeout_duration = 1.0  # Time (in seconds) before stopping motion
+        self.timer = self.create_timer(0.5, self.check_timeout)
+
+
+    def pixel_callback(self, msg: Point):
+        self.last_msg_time = self.get_clock().now()  # Update last received message time
+
+        pix_error = msg.x - msg.y
         twist = Twist()
         twist.linear.x = 0.0
 
-        if msg.x < msg.y - 50:
-            twist.angular.z = 0.5
-        elif msg.x > msg.y + 50:
-            twist.angular.z = -0.5
+        if abs(pix_error) > self.dead_zone:  # ignore small errors
+            twist.angular.z = -self.Kp * pix_error
+            twist.angular.z = max(min(twist.angular.z, self.max_angular_speed), -self.max_angular_speed)
         else:
-            twist.angular.z = 0.0
+            twist.angular.z = 0.0  # stop rotation if error is small
             
         self._vel_publish.publish(twist)
+        
+    def check_timeout(self): 
+        """ Stop rotation if no new message is received for `timeout_duration` seconds. """
+        time_since_last_msg = (self.get_clock().now() - self.last_msg_time).nanoseconds / 1e9  # Convert to seconds
+        if time_since_last_msg > self.timeout_duration:
+            twist = Twist()
+            twist.angular.z = 0.0  # Stop rotation
+            self._vel_publish.publish(twist)
+
 
 def main():
     rclpy.init()
